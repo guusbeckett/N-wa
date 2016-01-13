b358df240d509230069da90fa7417183dd6899a4
Kraegon <Jgwest@zaku2.com>
2016-01-13 20:43:50 +0100

    Added a title screen menu Here goes! Ultimate Secret Technique, Git Series, Massive Project Breaking Commit! Take this, my love, my anger, and all of my sorrow!

---
+++ Assets/_Scripts/Menu_scripts/Pulse_Title.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Pulse_Title : MonoBehaviour {
+    //Text to pulsate
+    public GameObject TitleText;
+
+    //Local variables
+    float pulse;
+    bool increasing = true;
+
+    // Causes to pulse between limits 1.0f and 0.0f
+    void Update() {
+        float h, s, v;
+        Color.RGBToHSV(TitleText.GetComponent<Text>().color, out h, out s, out v);
+        s = pulse;
+        TitleText.GetComponent<Text>().color = Color.HSVToRGB(h, s, v);
+        pulse = (increasing) ? pulse + 0.01f : pulse - 0.01f;
+        increasing = (increasing && (pulse >= 1.0f)) ? false : increasing;
+        increasing = (!increasing && (pulse < 0.0f)) ? true : increasing;
+	}
+}
